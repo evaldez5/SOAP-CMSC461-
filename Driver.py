f5d933325dae05615e08942874597fde2c9cc769
Jack Wang <wzn1234234@gmail.com>
2019-05-01 19:56:45 -0400

    need to add load a file into table, and print out data from select command

--- Driver.py
+++ Driver.py
@@ -1,6 +1,5 @@
 '''This file is used to be the driver for SOAP database
 '''
-import json
 import pymysql
 
 # Connection generator in one place for easy adjustment later
@@ -15,6 +14,27 @@ connection = getConn()
 cursor = connection.cursor()
 inputCommand = ""
 
+
 while inputCommand != "quit":
-    inputCommand = input("What is next command? ")
-    cursor.execute(inputCommand)
+    try:
+        inputCommand = input("What is next command? ")
+        if inputCommand != "quit":
+            if inputCommand.split(' ', 1)[0] == "load":
+                '''load format: load fileName into tableName'''
+                fileName = inputCommand.split(' ', 1)[1]
+                tableName = inputCommand.split(' ', 1)[3]
+                #todo load from a csv file
+            elif inputCommand.split(' ', 1)[0] == "select":
+                cursor.execute(inputCommand)
+                rows_s = str(cursor.fetchall())
+                #todo format data and print out
+            else:
+                cursor.execute(inputCommand)
+    except:
+        print("Wrong SQL Syntax, try again")
+        connection.rollback()
+
+
+cursor.close()
+connection.close()
+
