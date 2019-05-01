43f04e39d4554934a5cee0289e40ed181cdda8ae
Jack Wang <wzn1234234@gmail.com>
2019-05-01 19:07:21 -0400

    add simple driver to handle the database

---
+++ Driver.py
@@ -0,0 +1,19 @@
+'''This file is used to be the driver for SOAP database
+'''
+import json
+import pymysql
+
+# Connection generator in one place for easy adjustment later
+def getConn():
+    return pymysql.connect(host='localhost',
+                           user='root',
+                           password='team4our++',
+                           db='SOAP',
+                           autocommit=True)
+
+connection = getConn()
+cursor = connection.cursor()
+
+while inputCommand != "quit":
+    inputCommand = input("What is next command? ")
+    cursor.execute(inputCommand)
